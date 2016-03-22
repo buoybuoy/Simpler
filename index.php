46cb578697936ec1c2526279eb18b3667de075b4
buoybuoy <brianjamesbuie@gmail.com>
2016-03-21 22:48:31 -0500

    basic modal working

--- index.php
+++ index.php
@@ -22,7 +22,9 @@ include('template/partials/_titlebar.php');
 				foreach($view->transactions as $transaction){
 					echo '<tr>';
 					echo '<td class="date">' . date("M j", strtotime($transaction['date'])) . '</td>';
-					echo '<td>' . $transaction['description'] . '</td>';
+					echo '<td>';
+					echo '<a href="#" data-toggle="modal" data-target="#transactionModal" data-transaction="'. $transaction['id'] . '">';
+					echo $transaction['description'] . '</a></td>';
 					if ($transaction['transaction_type'] == "debit"){
 						echo '<td class="align-right negative">' . number_format($transaction['amount'], 2, '.', '') . '<span></td>';
 					} else {
@@ -42,6 +44,26 @@ include('template/partials/_titlebar.php');
 	</div>
 </div>
 
+
+<a href="#" data-toggle="modal" data-target="#transactionModal" data-transaction="the id">test</a>
+
+<div class="modal fade" id="transactionModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="exampleModalLabel">Transaction </h4>
+      </div>
+      <div class="modal-body">
+      	<h5 class="transaction" id="transactionID">something</h5>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Save</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
